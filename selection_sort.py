c3c76de2eeb4d5c3896e1aba0b2943e46906ff02
Amit Rawat <georgianamitrawat@gmail.com>
2018-10-19 19:12:35 +0530

    completed the selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,1 +1,16 @@
+def selection_sort(ls):
+    for i in range(len(ls)):
+        min = i
+        for j in range(i+1,len(ls)):
+            if ls[min] > ls[j]:
+                min = j
 
+        ls[i],ls[min] = ls[min],ls[i]
+
+def print_list(ls):
+    for i in ls:
+        print(i, end=" ")
+        
+ls = [34, 65, 26, 65, 59]
+selection_sort(ls)
+print_list(ls)
